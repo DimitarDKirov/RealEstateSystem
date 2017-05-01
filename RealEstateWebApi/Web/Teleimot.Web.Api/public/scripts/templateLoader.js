45b02f8b0d947b08df8192f2c3338a603c568d99
DimitarDKirov <dimkirov@gmail.com>
2017-05-01 23:58:52 +0300

    move index.html to root folder, added static files handler for .handlebars files

--- RealEstateWebApi/Web/Teleimot.Web.Api/public/scripts/templateLoader.js
+++ RealEstateWebApi/Web/Teleimot.Web.Api/public/scripts/templateLoader.js
@@ -1,6 +1,6 @@
 function loadTemplate(name) {
     return new Promise(function(resolve, reject) {
-        $.get('templates/' + name + '.html')
+        $.get('/public/templates/' + name + '.html')
             .done((file) => {
                 let template = Handlebars.compile(file);
                 resolve(template);
