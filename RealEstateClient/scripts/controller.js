7136ff030e1417304eb0e51a1b1bf8c8308481a3
DimitarDKirov <dimkirov@gmail.com>
2017-05-01 17:01:47 +0300

    update client

--- RealEstateClient/scripts/controller.js
+++ RealEstateClient/scripts/controller.js
@@ -1,9 +1,60 @@
 let controller = {
     home() {
-        toastr.error("test toastr");
+        var realEstates;
+        data.realEstates()
+            .then(estates => {
+                realEstates = estates;
+                return loadTemplate('home');
+            })
+            .then(template => {
+                var html = template(realEstates);
+                console.log(realEstates);
+                $('#content').html(html);
+            })
+            .catch(console.log);
     },
     login() {
+        loadTemplate('userLogin')
+            .then((template) => {
+                var html = template();
+                $('#content').html(html);
 
+                $("#btn-login").on("click", (ev) => {
+                    let userData = {
+                        username: $("#username").val(),
+                        password: $("#password").val()
+                    };
+
+                    data.login(userData)
+                        .then((userDetails) => {
+                            userLoggedIn();
+                            router.navigate('/');
+                        })
+                        .catch(console.log);
+
+                    ev.preventDefault();
+                    return false;
+                });
+
+                $("#btn-register").on("click", (ev) => {
+                    let userData = {
+                        "username": $("#username").val(),
+                        "password": $("#password").val()
+                    };
+
+                    data.register(userData)
+                        .then((userName) => {
+                            return data.login(userData);
+                        })
+                        .then((userDetails) => {
+                            userLoggedIn();
+                            router.navigate('/');
+                        })
+                        .catch(console.log);
+                    ev.preventDefault();
+                    return false;
+                });
+            });
     },
     logout() {
 
--- RealEstateClient/scripts/data.js
+++ RealEstateClient/scripts/data.js
@@ -1,6 +1,30 @@
 //const HTTP_HEADER_KEY = "x-auth-key";
 
 let data = {
+    realEstates() {
+        //return request.getJSON('api/RealEstates/');
+        return Promise.resolve({
+            result: [
+                {
+                    "Id": 2,
+                    "Title": "My house is for sale!",
+                    "SellingPrice": 28000,
+                    "RentingPrice": null,
+                    "CanBeSold": true,
+                    "CanBeRented": false
+                },
+                {
+                    "Id": 1,
+                    "Title": "Some very interesting office",
+                    "SellingPrice": 120000,
+                    "RentingPrice": 500,
+                    "CanBeSold": true,
+                    "CanBeRented": true
+                }
+            ]
+        }
+        );
+    },
     login(userData) {
         var result = request.putJSON('api/users/auth/', userData)
             .then(usrDetails => {
